4a72a7922a9a04784c2eff0f8cf11878cf2a2855
1qza112 <111991891+1qza112@users.noreply.github.com>
2022-08-29 21:59:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -51,9 +51,9 @@ def get_weather(region):
         # 获取地区的location--id
         location_id = response["location"][0]["id"]
     weather_url = "https://devapi.qweather.com/v7/weather/now?location={}&key={}".format(location_id, key)
-    weather_url1 = "https://devapi.qweather.com/v7/weather/3d?location={}&key={}".format(location_id, key)
+    #weather_url1 = "https://devapi.qweather.com/v7/weather/3d?location={}&key={}".format(location_id, key)
     response = get(weather_url, headers=headers).json()
-    response1 = get(weather_url1, headers=headers).json()
+   # response1 = get(weather_url1, headers=headers).json()
     # 天气
     weather = response["now"]["text"]
     # 当前温度
@@ -61,7 +61,7 @@ def get_weather(region):
     # 风向
     wind_dir = response["now"]["windDir"]
     #预报当天最高温度
-    tempMax=response1["daily"]["tempMax"]
+    tempMax=response1["now"]["feelsLike"]
     return weather, temp, wind_dir,tempMax
  
  
